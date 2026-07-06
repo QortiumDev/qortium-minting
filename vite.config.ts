84b94010938370b31581035d64d84671bc54505f
QuickMythril <quickmythril@protonmail.com>
2026-07-06 16:46:29 -0400

    app: add Minting version badge (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+import { readFileSync } from 'node:fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8')) as {
+  version: string;
+};
 
 export default defineConfig({
   base: './',
   build: {
     assetsInlineLimit: (filePath) =>
       /[/\\]src[/\\]assets[/\\]fonts[/\\].+\.ttf$/.test(filePath) ? true : undefined,
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(`v${packageJson.version}`),
   },
   plugins: [react()],
   test: {
